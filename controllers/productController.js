7e1a9445512b88d1ef2662f5d692a1472ba4a0e1
Abhishek Anand <120253524+anand-1Abhishek@users.noreply.github.com>
2023-11-03 23:03:54 +0530

    mark 5

--- controllers/productController.js
+++ controllers/productController.js
@@ -64,12 +64,10 @@ export const createProductController = async (req, res) => {
 //get all products
 export const getProductController = async (req, res) => {
   try {
-    const products = await productModel
-      .find({})
-      .populate("category")
-      // .select("-photo")
-      .limit(12)
-      .sort({ createdAt: -1 });
+    const products = await productModel.find({}).populate("category")
+      // // .select("-photo")
+      // .limit(12)
+      // .sort({ createdAt: -1 });
     res.status(200).send({
       success: true,
       counTotal: products.length,
